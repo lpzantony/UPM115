00aa9c15291846733462b346ace96cff399d3f1b
Antony Lopez <antony.lopez@telecom-paristech.fr>
2017-11-18 21:33:56 +0100

    WIP : ReadMe & schematics

---
+++ Wifi/Wifi.ino
@@ -0,0 +1,85 @@
+/**
+ * @example TCPClientSingle.ino
+ * @brief The TCPClientSingle demo of library WeeESP8266. 
+ * @author Wu Pengfei<pengfei.wu@itead.cc> 
+ * @date 2015.02
+ * 
+ * @par Copyright:
+ * Copyright (c) 2015 ITEAD Intelligent Systems Co., Ltd. \n\n
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version. \n\n
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "ESP8266.h"
+
+#define SSID        "Athens2016"
+#define PASSWORD    "Arduino2016"
+#define HOST_NAME   "192.168.0.129"
+#define HOST_PORT   22522
+
+SoftwareSerial mySerial(7,6);
+
+ESP8266 wifi(mySerial, 9600);
+
+void setup(void)
+{
+    Serial.begin(9600);
+    Serial.print("setup begin\r\n");
+    
+    Serial.print("FW Version:");
+    Serial.println(wifi.getVersion().c_str());
+      
+    if (wifi.setOprToStation()) {
+        Serial.print("to station + softap ok\r\n");
+    } else {
+        Serial.print("to station + softap err\r\n");
+    }
+ 
+    if (wifi.joinAP(SSID, PASSWORD)) {
+        Serial.print("Join AP success\r\n");
+        Serial.print("IP:");
+        Serial.println( wifi.getLocalIP().c_str());       
+    } else {
+        Serial.print("Join AP failure\r\n");
+    }
+    
+    if (wifi.disableMUX()) {
+        Serial.print("single ok\r\n");
+    } else {
+        Serial.print("single err\r\n");
+    }
+    
+    Serial.print("setup end\r\n");
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+ 
+void loop(void)
+{
+    uint8_t buffer[128] = {0};
+    
+    if (wifi.createTCP(HOST_NAME, HOST_PORT)) {
+        Serial.print("Connected to server \n");
+    } else {
+        Serial.print("Disconnected from server \n");
+    }
+    
+    uint32_t len = wifi.recv(buffer, sizeof(buffer), 10000);
+    if (len > 0) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.print("Received:[");
+        for(uint32_t i = 0; i < len; i++) {
+            Serial.println((int)buffer[i]);//Le buffer contient la valeur : 1 = z / 2 = q / 3 = s /4 = d
+        }
+        Serial.print("]\r\n");
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+}
+     
